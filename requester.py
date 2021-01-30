f2045a3a7000d4798bd3a9767065e5d9ac8f6c75
Jack <culbert.jack+git@gmail.com>
2021-01-30 19:19:39 +0000

    Improved restart capabilities

--- requester.py
+++ requester.py
@@ -1,17 +1,17 @@
-import requests
-from pathlib import Path
 import json
 import datetime
 import time
 import logging
 import threading
+from pathlib import Path
 
-import tqdm
+import requests
+from tqdm import tqdm
 
 class chan4requester():
     def __init__(self, monitor, include_boards = None, exclude_boards = None, request_time_limit = 1, stream_log_level = logging.INFO, logfolderpath = 'logs'):
         self._base_save_path = Path().resolve()
-        self.debuglog = False
+        self._debuglog = False
         self._stream_log_level = logging.INFO
         self._setup_logging(logfolderpath)
 
@@ -31,7 +31,7 @@ class chan4requester():
         self.monitor = True
         self.monitoring_boards = []
         self.monitoring_threads = {}
-        self._logger.debug("Initialising Moitoring Thread")
+        self._logger.debug("Initialising Monitoring Thread")
         self._monitor_thread = threading.Thread(target=self._begin_monitoring)
         self._logger.debug("Starting Thread")
         self._monitor_thread.start()
@@ -49,17 +49,32 @@ class chan4requester():
         old_monitor_dict = {}
         old_threads = 0
         for board in self.monitoring_boards:
-            old_monitor_dict[board] = {}
             timestamp = self._get_day()
-            outpath = self._base_save_path / 'saves' / timestamp / 'threads' / board
-            outpath.mkdir(parents=True, exist_ok=True)
-            old_monitors = []
-            for previous_thread_files in outpath.iterdir():
-                old_monitors.append(previous_thread_files.name.split("_")[0])
-            old_monitors = list(set(old_monitors))
-            for monitor in old_monitors:
-                old_monitor_dict[board][monitor] = [0,0]
-            old_threads += len(old_monitors)
+
+            boardfolderpath = self._base_save_path / 'saves' / timestamp / 'threads_on_boards'
+            boardfolderpath.mkdir(parents=True, exist_ok=True)
+            threadpath = self._base_save_path / 'saves' / timestamp / 'threads' / board
+            threadpath.mkdir(parents=True, exist_ok=True)
+
+            good_boardpath_found = False
+            for boardpath in boardfolderpath.iterdir():
+                if boardpath.name.split("_")[0] == str(board):
+                    good_boardpath = boardpath
+                    good_boardpath_found = True
+
+            if not good_boardpath_found:
+                self._logger.info(f'No previous thread information for /{board}/, no old threads to monitor')
+                continue
+            old_monitor_dict[board] = {}
+
+            with open(good_boardpath, 'r') as prev_threads_file:
+                prev_threads = json.load(prev_threads_file)
+                for page in prev_threads:
+                    for threads in page['threads']:
+                        old_monitor_dict[board][threads['no']] = [threads['last_modified'], threads['replies']]
+                        old_threads += 1
+            self._logger.debug(f'{len} past captures of old threads in previous instances discovered')    
+
         self.monitoring_threads = old_monitor_dict
         self._logger.debug(f'{old_threads} past captures of old threads in previous instances discovered')
 
@@ -147,11 +162,11 @@ class chan4requester():
     def _update_posts_on_monitoring_threadlist(self):
         number_posts_in_iteration = len(self._posts_to_update)
         i = 1
-        for board, post in self._posts_to_update:
+        for board, post in tqdm(self._posts_to_update):
             start_time = time.time()
             self.get_and_save_thread(board, post)
             current_time_diff = (time.time() - start_time) * (number_posts_in_iteration - i)
-            self._logger.info(f'{i}/{number_posts_in_iteration}: Capturing post {post} in /{board}/ approximate seconds remaining in iteration {current_time_diff:n}')
+            self._logger.debug(f'{i}/{number_posts_in_iteration}: Capturing post {post} in /{board}/ approximate seconds remaining in iteration {current_time_diff:n}')
             i += 1
 
     def _set_board_list(self):
@@ -209,7 +224,7 @@ class chan4requester():
             filename = 'boards.json'
         outpath.mkdir(parents=True, exist_ok=True)
         with open(outpath / filename, 'w') as outfile:
-            json.dump(self.get_chan_info_json(), outfile)
+            json.dump(self.get_chan_info_json(), outfile, indent=2)
 
     def get_and_save_single_board_threadlist(self, board_code, outpath=None, filename=None, with_return=False):
         timestamp = self._get_day()
@@ -271,7 +286,7 @@ class chan4requester():
         self._infologfile.setFormatter(self._log_formatter)
         self._logger.addHandler(self._infologfile)
 
-        if self.debuglog:
+        if self._debuglog:
             self._debuglogpath = logfolder / ('debug_log' + self._get_full_time() + '.log')
             self._debuglogfile = logging.FileHandler(self._debuglogpath)
             self._debuglogfile.setLevel(logging.DEBUG)
